852062897628f61be75f7efbaae462c1e4018619
edson <firstedson@gmail.com>
2023-02-25 13:35:49 +0300

    solved viewing templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,13 +1,22 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
+from django.utils import timezone
+
+
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request,'blog/post_list.html', {'posts': posts})
 
 
-
+# def current_datetime(request):
+#     now = datetime.datetime.now()
+#     html = "<html><body>It is now %s.</body></html>" % now
+#     return HttpResponse(html)
 
 
 
