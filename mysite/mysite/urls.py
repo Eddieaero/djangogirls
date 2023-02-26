dc411ae07d08d851f57bb4ecd5a20b9a1c44bba6
edson <firstedson@gmail.com>
2023-02-26 16:31:06 +0300

    seecond attempt

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     # path('advanced-admin/', advanced_site.urls),
 ]
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
